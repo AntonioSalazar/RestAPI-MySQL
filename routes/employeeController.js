e35be0c328e04525cacd6a522c8e251a5fada8c4
Salazar <antonio.salazar@oliverwyman.com>
2019-04-29 13:37:27 -0500

    able to get an employee by ID and post a new employee to DB

--- routes/employeeController.js
+++ routes/employeeController.js
@@ -19,6 +19,40 @@ function listAllEmployees(req, res) {
     .catch(err => res.status(500).json(err))
 }
 
+function listOneEmployee(req, res) {
+  const { knex } = req.app.locals;
+  const { id } = req.params;
+  knex.select('name', 'address', 'email', 'hired', 'dob', 'salary', 'bonus', 'photo', 'department')
+    .from('employees')
+    .where({ id: `${id}`})
+    .then(infoFromDB => {
+      if (infoFromDB.length > 0) {
+        return res.status(200).json(infoFromDB)
+      } else {
+        return res.status(404).json(`employee with ${id} ID not found`)
+      }
+    })
+    .catch(err => res.status(500).json(err))
+}
+
+function createEmployee(req, res) {
+  const {knex} = req.app.locals;
+  const payload = req.body;
+  const mandatoryColumns = ['name', 'email', 'salary'];
+  const payloadKeys = Object.keys(payload);
+  const mandatoryColumnsExists = mandatoryColumns.every(mandatoryCols => payloadKeys.includes(mandatoryCols))
+  if (mandatoryColumnsExists) {
+    knex('employees')
+      .insert(payload)
+      .then(newEmployeeID => res.status(200).json(`Employee created with the id: ${newEmployeeID}`))
+      .catch(err => res.status(500).json(err)); 
+  } else {
+    return res.status(400).json(`Mandatory columns required: ${mandatoryColumns}`)
+  }
+}
+
 module.exports = {
-  listAllEmployees
+  listAllEmployees,
+  listOneEmployee,
+  createEmployee
 } ;
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router  = express.Router();
-const routes  = require('./routes')
+const routes  = require('./routes');
+const middlewares = require('../middlewares/middlewares')
 
 
 router.get('/employees', routes.employees.listAllEmployees);
+
+router.get('/employees/:id',  middlewares.getIDAsInteger, routes.employees.listOneEmployee);
+
+router.post('/employees', routes.employees.createEmployee);
 
 module.exports = router;
